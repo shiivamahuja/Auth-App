2b4608a6a7db67a3904cec7f4f6201fc22e64c0c
Shivam Ahuja <shiivamahuja@gmail.com>
2026-02-03 19:20:18 +0530

    Splash Screen Logic Added

--- app/src/main/java/com/doradoradevelopers/authapp/MainActivity.kt
+++ app/src/main/java/com/doradoradevelopers/authapp/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.doradoradevelopers.authapp.ui.theme.AuthAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +16,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AuthAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Navigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AuthAppTheme {
-        Greeting("Android")
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "splashScreen") {
+        composable("splashScreen") {
+            SplashScreen(navController)
+        }
+        composable("loginScreen") {
+            LoginScreen(navController)
+        }
+        composable("registerScreen") {
+            RegisterScreen(navController)
+        }
     }
 }
